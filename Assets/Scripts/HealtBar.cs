55af8b52a94456cc1c0797fbc5c4c078a4aba784
Dmitriy Kulyasov <vvvvvvmaz@gmail.com>
2022-06-12 18:48:14 +0300

    fix drowing bar

--- Assets/Scripts/HealtBar.cs
+++ Assets/Scripts/HealtBar.cs
@@ -6,14 +6,46 @@ using UnityEngine.UI;
 
 public class HealtBar : MonoBehaviour
 {
-    [SerializeField] private Health _health;
     [SerializeField] private Slider _healtBar;
-    
+    [SerializeField] private Health _health;
+
+    private const float _minimalStep = 0.02f;
+
+    private void Start()
+    {
+        _healtBar.value = _health.HealthValue;
+    }
+
     private void Update()
     {
-        if (_health.IsHealthChange)
+        if(_healtBar.value > _health.HealthValue)
         {
-            _healtBar.value = _health.HealthValue;
+            TakeDamage(_healtBar.value - _health.HealthValue);
+        }
+        else if(_healtBar.value < _health.HealthValue)
+        {
+            Heal(_health.HealthValue - _healtBar.value);
+        }
+    }
+
+    private void Heal(float difference)
+    {
+        float target = Mathf.Clamp(_healtBar.value + difference, _healtBar.minValue, _healtBar.maxValue);
+        StartCoroutine(HpChange(target));
+    }
+
+    private void TakeDamage(float difference)
+    {
+        float target = Mathf.Clamp(_healtBar.value - difference, _healtBar.minValue, _healtBar.maxValue);
+        StartCoroutine(HpChange(target));
+    }
+
+    private IEnumerator HpChange(float target)
+    {
+        while (_healtBar.value != target)
+        {
+            _healtBar.value = Mathf.MoveTowards(_healtBar.value, target, _minimalStep);
+            yield return null;
         }
     }
 }
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -9,41 +9,24 @@ public class Health : MonoBehaviour
     [SerializeField] private Button _hPDecrease;
 
     private const float _step = 2f;
-    private const float _minimalStep = 0.02f;
     private const float _minValueHP = 0;
     private const float _maxValueHP = 20f;
 
-    public float HealthValue { get; private set; }
-    public bool IsHealthChange { get; private set; }
+    public float HealthValue { get; private set; } = _maxValueHP / 2f;
 
     private void Start()
     {
-        HealthValue = _maxValueHP / 2f;
         _hPIncrease.onClick.AddListener(Heal);
         _hPDecrease.onClick.AddListener(TakeDamage);
     }
 
     private void Heal()
     {
-        float target = Mathf.Clamp(HealthValue + _step, _minValueHP, _maxValueHP);
-        StartCoroutine(HpChange(target));
+        HealthValue = Mathf.Clamp(HealthValue + _step, _minValueHP, _maxValueHP);
     }
 
     private void TakeDamage()
     {
-        float target = Mathf.Clamp(HealthValue - _step, _minValueHP, _maxValueHP);
-        StartCoroutine(HpChange(target));
-    }
-
-    private IEnumerator HpChange(float target)
-    {
-        IsHealthChange = true;
-
-        while (HealthValue != target)
-        {
-            HealthValue = Mathf.MoveTowards(HealthValue,target, _minimalStep);
-            yield return null;
-        }
-        IsHealthChange = false;
+        HealthValue = Mathf.Clamp(HealthValue - _step, _minValueHP, _maxValueHP);
     }
 }

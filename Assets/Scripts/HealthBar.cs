4da2523b4f8322e141c234e521ddf84c6392de5a
Dmitriy Kulyasov <vvvvvvmaz@gmail.com>
2022-06-13 14:27:56 +0300

    fix

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealtBar : MonoBehaviour
+public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Slider _healthBar;
     [SerializeField] private Health _health;
